54f07018fe20edf05b2b63d726e54a6da26064ae
Krzysztof Piotrowicz <krzychu2510@gmail.com>
2018-06-25 19:29:31 +0200

    working product download and page

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -19,5 +19,27 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+var GLOBALS_siteUrl = "http://localhost:9069/api/"
+function getProducts(){
+    $.ajax({
+        method: "GET",
+        url: GLOBALS_siteUrl+"products",
+    }).done(function(response) {
+        loadProducts(response);
+    });
+}
+
+function loadProducts(data){
+    for(var i = 0; i<data.length; i++){
+        var div = document.createElement("div");
+        div.className = "productsItem";
+        var img = document.createElement("img");
+        div.appendChild(img);
+        var name = document.createElement("h2");
+        name.innerHTML = data[i].name + " Cena:"+data[i].price+" zł";
+        div.appendChild(name);
+        document.getElementById("products1").appendChild(div);
+    }
+}
 
 app.initialize();
